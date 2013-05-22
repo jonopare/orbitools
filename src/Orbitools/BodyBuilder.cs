baded20ddfbdac7047376b7128865690b7eaf742
jono.pare@gmail.com <jono.pare@gmail.com@cf541d02-5e20-713c-669b-f510489dc13d>
2013-05-22 18:43:59 +0000

    Added first draft of source code

---
+++ src/Orbitools/BodyBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orbitools
+{
+    public class BodyBuilder
+    {
+        private Triplet position;
+        private Triplet direction;
+        private double speed;
+        private double mass;
+
+        public BodyBuilder Mass(double mass)
+        {
+            this.mass = mass;
+            return this;
+        }
+
+        public BodyBuilder Speed(double speed)
+        {
+            this.speed = speed;
+            return this;
+        }
+
+        public BodyBuilder Direction(Triplet direction)
+        {
+            this.direction = direction.Unit;
+            return this;
+        }
+
+        public BodyBuilder Direction(double x, double y, double z)
+        {
+            return Direction(new Triplet(x, y, z));
+        }
+
+        public BodyBuilder Position(Triplet position)
+        {
+            this.position = position;
+            return this;
+        }
+
+        public BodyBuilder Position(double x, double y, double z)
+        {
+            return Position(new Triplet(x, y, z));
+        }
+
+        public Body ToBody()
+        {
+            return new Body(mass, position, direction * speed);
+        }
+    }
+}
---
+++ src/Orbitools/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Orbitools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jono Pare")]
+[assembly: AssemblyProduct("Orbitools")]
+[assembly: AssemblyCopyright("Copyright © Jono Pare 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
