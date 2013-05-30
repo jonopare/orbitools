489e36ba75dffcf2c4010a4558fb83bd817b4477
jono.pare@gmail.com <jono.pare@gmail.com@cf541d02-5e20-713c-669b-f510489dc13d>
2013-05-30 19:43:45 +0000

    Added a model to encapsulate n-body orbits, as well as a static class of Solar System objects and some minor additions to body and triplet.

---
+++ src/Orbitools/BodyEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orbitools
+{
+    public class BodyEventArgs : EventArgs
+    {
+        public Body Body { get; private set; }
+
+        public BodyEventArgs(Body body)
+        {
+            Body = body;
+        }
+    }
+}
