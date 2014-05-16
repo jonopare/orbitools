2b7f8ed7024c2b427f34fee9bc122e5b215c0921
jono.pare@gmail.com <jono.pare@gmail.com@cf541d02-5e20-713c-669b-f510489dc13d>
2014-05-16 21:59:19 +0000

    Added gravity UI demo (needs to be refactored into orbitools)

---
+++ trunk/src/Gravity/SolarSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gravity
+{
+    public class SolarSystem
+    {
+        public readonly static IBody Sun = new Planet(132712440018, Vector3.Zero, 1);
+        public readonly static IBody Mercury = new Planet(22032, new Vector3(58910000, 0, 0), 88.969);
+        public readonly static IBody Venus = new Planet(324859, new Vector3(108200000, 0, 0), 224.7);
+        public readonly static IBody Earth = new Planet(398600.4418, new Vector3(149600000, 0, 0), 365.25);
+        //public readonly static IBody Moon = new Planet(4902.8);
+        public readonly static IBody Mars = new Planet(42828, new Vector3(227900000, 0, 0), 687);
+        //public readonly static IBody Ceres = new Planet(63.1);
+        public readonly static IBody Jupiter = new Planet(126686534, new Vector3(778500000, 0, 0), 4333);
+        public readonly static IBody Saturn = new Planet(37931187, new Vector3(1433000000, 0, 0), 10832);
+        public readonly static IBody Uranus = new Planet(5793939, new Vector3(2877000000, 0, 0), 30667);
+        public readonly static IBody Neptune = new Planet(6836529, new Vector3(4503000000, 0, 0), 60290);
+        //public readonly static IBody Pluto = new Planet(871);
+        //public readonly static IBody Eris = new Planet(1108);
+    }
+}
---
+++ trunk/src/Gravity/Vector3.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gravity
+{
+    public class Vector3
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double Length { get { return Math.Sqrt(LengthSquared); } }
+        public double LengthSquared { get { return X * X + Y * Y + Z * Z; } }
+
+        public static Vector3 operator +(Vector3 left, Vector3 right)
+        {
+            return new Vector3(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+        }
+
+        public static Vector3 operator -(Vector3 left, Vector3 right)
+        {
+            return new Vector3(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+        }
+
+        public static Vector3 operator /(Vector3 vector, double scalar)
+        {
+            return new Vector3(vector.X / scalar, vector.Y / scalar, vector.Z / scalar);
+        }
+
+        public static Vector3 operator *(Vector3 vector, double scalar)
+        {
+            return new Vector3(vector.X * scalar, vector.Y * scalar, vector.Z * scalar);
+        }
+
+        public static Vector3 operator *(double scalar, Vector3 vector)
+        {
+            return vector * scalar;
+        }
+
+        public readonly static Vector3 Zero = new Vector3(0, 0, 0);
+
+
+        public Vector3 Unit
+        {
+            get { return this / Length; }
+        }
+
+        public override string ToString()
+        {
+            return new StringBuilder("X=").Append(X).Append(", Y=").Append(Y).Append(", Z=").Append(Z).ToString();
+        }
+    }
+
+
+}
